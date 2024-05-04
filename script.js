b57fd8492f9bfb8ea61c71ebaaf08ae7ca0f4813
Abi Garner <abigail.gee@hotmail.com>
2024-05-04 01:15:00 -0400

    added new function and changed values in html

--- script.js
+++ script.js
@@ -15,14 +15,20 @@ animImg.addEventListener("mouseout", event => {
 });
 
 ////////////////////////////////////////////////
-const cakeCheck = document.querySelectorAll('input[type="checkbox"]:checked');
+// const cakeCheck = document.querySelectorAll('input[value]:checked');
 // console.log(cakeCheck);
 ///////// confirming with user if preferences are correct
 const cakeForm = document.getElementById("flavorForm");
+const values = [];
+const elements = cakeForm.querySelectorAll('input[type="checkbox"]:checked');
+
 cakeForm.addEventListener("submit", event => {
+    event.preventDefault();
+    elements.forEach(InputEvent =>{values.push(element.value)});
+
     let text= "Are the preferences you selected correct?"
     if(confirm(text)==true){
-        console.log(cakeCheck)
+        console.log(values)
     }else{
         console.log(null)
     }
